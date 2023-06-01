bdea9484c30e033bf6ffbc52c5d5e5d2c2ea012a
Vedant Jain <vedant.jain.indore@gmail.com>
2023-06-01 21:34:03 +0530

    Day 5 Linked List

---
+++ LinkedList_Reverse_LL.cpp
@@ -0,0 +1,25 @@
+Node* reverseusingLoop(Node* head ) {
+        Node* prev = NULL;
+        Node* curr = head;
+
+        while(curr != NULL ) {
+                Node* temp = curr ->next;
+                curr ->next = prev;
+                prev = curr;
+                curr = temp;
+        }
+        return prev;
+}
+Node* reverseusingRecursion(Node* prev, Node* curr) {
+        //base case
+        if(curr == NULL) 
+                return prev;
+
+        Node* temp = curr ->next;
+        curr ->next = prev;
+        prev = curr;
+        curr = temp;
+
+        //recursion sambhal lega
+        return reverseusingRecursion(prev, curr);
+}
