0fb200e45e4f4653b86835d359a4a7a2fa523da2
Vedant Jain <vedant.jain.indore@gmail.com>
2023-07-01 17:56:17 +0530

    Day 25 DP

--- DP_Matrix_Chain_Multiplication.cpp
+++ DP_Matrix_Chain_Multiplication.cpp
@@ -1,55 +1,78 @@
-// Memoisation
+// Tabulation
 
- #include <bits/stdc++.h>
-using namespace std;
+int mcm(vector<int>&arr, int N){
+    int dp[N][N];
 
-int f(vector<int>& arr, int i, int j, vector<vector<int>>& dp){
-    
-    // base condition
-    if(i == j)
-        return 0;
-        
-    if(dp[i][j]!=-1)
-        return dp[i][j];
-    
-    int mini = INT_MAX;
-    
-    // partioning loop
-    for(int k = i; k<= j-1; k++){
-        
-        int ans = f(arr,i,k,dp) + f(arr, k+1,j,dp) + arr[i-1]*arr[k]*arr[j];
-        
-        mini = min(mini,ans);
-        
+    for(int i = 1; i < N; i++)
+        dp[i][i] = 0;
+
+    for(int i = N-1; i >= 1; i--){
+        for(int j = i+1; j < N; j++){
+            int mini = INT_MIN;
+
+            int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+
+            mini = min(steps, mini);
+        }
+        dp[i][j] = mini;
     }
     
-    return mini;
+    return dp[1][N-1];
 }
 
+// =============================================================================================================================================================================
+// Memoisation
 
-int matrixMultiplication(vector<int>& arr, int N){
-    
-    vector<vector<int>> dp(N,vector<int>(N,-1));
-    
-    int i =1;
-    int j = N-1;
-    
-    
-    return f(arr,i,j,dp);
-    
-    
-}
+//  #include <bits/stdc++.h>
+// using namespace std;
 
-int main() {
+// int f(vector<int>& arr, int i, int j, vector<vector<int>>& dp){
+    
+//     // base condition
+//     if(i == j)
+//         return 0;
+        
+//     if(dp[i][j]!=-1)
+//         return dp[i][j];
+    
+//     int mini = INT_MAX;
+    
+//     // partioning loop
+//     for(int k = i; k<= j-1; k++){
+        
+//         int ans = f(arr,i,k,dp) + f(arr, k+1,j,dp) + arr[i-1]*arr[k]*arr[j];
+        
+//         mini = min(mini,ans);
+        
+//     }
+    
+//     return mini;
+// }
+
+
+// int matrixMultiplication(vector<int>& arr, int N){
+    
+//     vector<vector<int>> dp(N,vector<int>(N,-1));
+    
+//     int i =1;
+//     int j = N-1;
+    
+    
+//     return f(arr,i,j,dp);
+    
+    
+// }
+
+// int main() {
 	
-	vector<int> arr = {10, 20, 30, 40, 50};
+// 	vector<int> arr = {10, 20, 30, 40, 50};
 	
-	int n = arr.size();
+// 	int n = arr.size();
 	
-	cout<<"The minimum number of operations is "<<matrixMultiplication(arr,n);
+// 	cout<<"The minimum number of operations is "<<matrixMultiplication(arr,n);
 	
-	return 0;
-}
+// 	return 0;
+// }
 
 
 // =============================================================================================================================================================================
